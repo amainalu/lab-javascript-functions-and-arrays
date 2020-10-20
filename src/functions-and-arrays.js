365f67da8b09087d9901d237fd8e71b0eac45c5a
amainar <amainalu@gmail.cm>
2020-10-20 15:24:55 +0200

    ongoing lab2

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,11 +1,33 @@
 // Iteration #1: Find the maximum
-
+function maxOfTwoNumbers(number1,number2){
+  if (number1>number2){
+    return number1;
+  } else if (number2>number1){
+    return number2;
+  } else if (number1===number2) {
+      return number1;
+    };
+  };
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
+function findLongestWord(words){
+  if (words.lenght===0){
+    return null;
+  } 
+  let longestWord="";
+    for (let i=0; i<words.length; i++){
+      if (words[i].length > longest.length)
+      longest = words[i];
+    }
+    return longestWord;
+  };
 // Iteration #3: Calculate the sum
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+numbers.forEach(function (sumNumbers){for (i=0; i<numbers.length;i=i++)
+  console.log(sumNumbers)
+  });
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
@@ -71,3 +93,13 @@ const matrix = [
   [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
+
+
+
+
+  
+
+
+
+
+  
